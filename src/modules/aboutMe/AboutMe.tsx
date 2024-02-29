07d625093c8d7b2d6d2068afac2b805d6ca8394c
jzahrndt11 <jerzahrndt@gmail.com>
2024-02-29 17:04:54 -0600

    Initial Commit

---
+++ src/modules/aboutMe/AboutMe.tsx
@@ -0,0 +1,11 @@
+
+
+export const AboutMe = () => {
+    
+    return (
+        <>
+            <div>About Me Page</div>
+        </>
+        
+    )
+}
---
+++ src/modules/pets/Pets.tsx
@@ -0,0 +1,13 @@
+
+export interface PetsProps {
+    title: string,
+    description: string,
+    imgScr: string
+}
+
+export const Pets = ({
+    title,
+    description,
+    imgScr
+}: PetsProps) => {
+}
